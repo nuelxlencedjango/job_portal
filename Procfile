c816897860a324e28174b94c6ba51b9487b359fb
nuelxelence <nuelxelence@gmail.com>
2021-12-19 20:08:05 +0100

    My first commit

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn iwan_work .wsgi
+
--- iwan_work/wsgi.py
+++ iwan_work/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iwan_work.settings')
 
 application = get_wsgi_application()
+
