f0e4f72a4194ced647655a685670d7e08d6e255c
nuelxelence <nuelxelence@gmail.com>
2022-03-24 19:24:32 +0100

    orderitems

--- products/views.py
+++ products/views.py
@@ -430,9 +430,9 @@ def payment_confirmation(request):
     order.ordered = True
     order.save()
 
-    orderitem = OrderItem.objects.filter(user=request.user, ordered=False,status='pending')
-    orderitem.ordered=True
-    orderitem.status ='paid'
+    orderitem = OrderItem.objects.filter(user=request.user, ordered=True,status='paid')
+    #orderitem.ordered=True ordered
+    #orderitem.status ='paid'
     orderitem.save()
 
     return redirect('products:handle_confirmation')
