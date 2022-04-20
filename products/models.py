f256fa71232c2e175b46d10524867550462738d7
nuelxelence <nuelxelence@gmail.com>
2022-04-20 11:47:37 +0100

    artisan profile pic

--- products/models.py
+++ products/models.py
@@ -10,7 +10,7 @@ from django.contrib.auth.models import AbstractUser
 
 from django.contrib.auth import get_user_model
 from django.shortcuts import reverse
-from artisan.models import *
+from artisan.models import Artisan
 
 User = get_user_model()
 
