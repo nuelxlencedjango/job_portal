bdc23d41b674287600692edb3dda7f30f22ea739
nuelxelence <nuelxelence@gmail.com>
2022-04-22 18:22:34 +0100

    artisan completed services

--- artisan/views.py
+++ artisan/views.py
@@ -116,7 +116,7 @@ def paidJobs(request):
 
 def jobDetail(request,id):
    # limit = 0
-    artisan = (Artisan.objects.filter(user=request.user))
+    artisan = [Artisan.objects.filter(user=request.user)]
     job_info= OrderItem.objects.get(id =id)
 
     #work here -conditions
