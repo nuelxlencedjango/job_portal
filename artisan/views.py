62b008db064e7824c8658ebb464d2806f90d8dc4
nuelxelence <nuelxelence@gmail.com>
2022-04-24 16:19:11 +0100

    artisan select jobs

--- artisan/views.py
+++ artisan/views.py
@@ -140,9 +140,8 @@ def jobAccepted(request,id):
 
         info = OrderItem.objects.get(id=id)   
 
-    for b in OrderItem.objects.filter(id =id):
-        
-        b.artisan_assigned.set(artisan)  
+        for b in info:
+            b.artisan_assigned.set(artisan)  
      
 
 
