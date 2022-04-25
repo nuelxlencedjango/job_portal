168a5f189a472341b2f7cc897cedfa8b84c92f36
nuelxelence <nuelxelence@gmail.com>
2022-04-25 08:24:31 +0100

    artisan select jobs

--- artisan/views.py
+++ artisan/views.py
@@ -142,8 +142,8 @@ def jobAccepted(request,id):
     for job in info:
         #for name in job:
         assigned =job.artisan_assigned.all()
-        artisan_assigned =assigned.artisan_assigned
-        
+        #artisan_assigned =assigned.artisan_assigned
+
         #OrderItem.objects.get(artisan_assigned=job.artisan_assigned.artisan.username).update()
 
         #job.artisan_assigned=job.user.username
