a8de26596ce9304604163b36fab6488c99e0bec8
nuelxelence <nuelxelence@gmail.com>
2022-04-25 08:15:53 +0100

    artisan select jobs

--- artisan/views.py
+++ artisan/views.py
@@ -141,7 +141,7 @@ def jobAccepted(request,id):
     info = OrderItem.objects.filter(id=id)  
     for job in info:
         #for name in job:
-        job.artisan_assigned =info.artisan_assigned.artisan.username
+        job.artisan_assigned =job.artisan_assigned.artisan.username
         context ={'job':job}
         #OrderItem.objects.get(artisan_assigned=job.artisan_assigned.artisan.username).update()
 
