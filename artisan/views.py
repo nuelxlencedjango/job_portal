d962348feb4225396e2c497bbbe776e825984a24
nuelxelence <nuelxelence@gmail.com>
2022-04-25 14:46:54 +0100

    artisan select jobs

--- artisan/views.py
+++ artisan/views.py
@@ -151,21 +151,21 @@ def jobAccepted(request,id):
     #id = job_info.id
     if OrderItem.objects.filter(id =id).exists():
         job_info= OrderItem.objects.filter(id =id)
-        for job in job_info:
+        for job in job_info.artisan_assigned.all():
             id = job.id
             #for assignee in job.artisan_assigned.all():
-            for assignee in job_info.artisan_assigned.all():
-                name = assignee.user.username
-                nin = assignee.nin
-                location=assignee.location
-                phone = assignee.phone
-                profession_name =assignee.profeesional_name
+            #for assignee in job_info.artisan_assigned.all():
+            name = job.user.username
+            nin = job.nin
+            location=job.location
+            phone = job.phone
+            profession_name =job.profeesional_name
                 #name=OrderItem.objects.filter(id =id,artisan_assigned=artisan).update()
 
                 #name.artisan_assigned =artisan
                 #name.save()
-                context ={'name':name,'nin':nin,'location':location,'phone':phone,'profession':profession_name,'assigned':assignee}        
-                return render(request,'artisans/accepted_job.html',context)       
+            context ={'name':name,'nin':nin,'location':location,'phone':phone,'profession':profession_name,'assigned':assignee}        
+            return render(request,'artisans/accepted_job.html',context)       
 
 
     context={'id':id}
