7e96eff3c6e81b7148f3cf15e660ccf850a7fc88
nuelxelence <nuelxelence@gmail.com>
2022-04-25 23:42:15 +0100

    artisan select jobs

--- artisan/views.py
+++ artisan/views.py
@@ -160,7 +160,7 @@ def jobAccepted(request,id):
     #if OrderItem.objects.filter(id =id).exists():
         OrderItem.objects.filter(id=id, ordered=True,status='Paid')
         accepted_job= OrderItem.objects.get(id=id, ordered=True,status='Paid')
-        OrderItem.objects.get(id=id, ordered=True,status='Paid').update(artisan_assigned=request.user)
+        OrderItem.objects.get(id=id, ordered=True,status='Paid').save()#update(artisan_assigned=request.user)
         #accepted_job.artisan_assigned =artisan
         #accepted_job.save()
       
