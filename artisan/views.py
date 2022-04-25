17df40080a1fcc72c416591028224887fa449cb3
nuelxelence <nuelxelence@gmail.com>
2022-04-25 15:18:59 +0100

    artisan select jobs

--- artisan/views.py
+++ artisan/views.py
@@ -155,7 +155,8 @@ def jobAccepted(request,id):
             id = job.id
             #for assignee in job.artisan_assigned.all():
             #for assignee in job_info.artisan_assigned.all():
-            name = job.artisan_assigned.assignee.user.username
+            for jab in job.artisan_assigned.all():
+                name = job.artisan_assigned.assignee.user.username
             nin = job.artisan_assigned.nin
             location=job.artisan_assigned.location
             phone = job.artisan_assigned.phone
