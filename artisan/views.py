95798f19ba14a23b10f2d9c07bc8acf17e902022
nuelxelence <nuelxelence@gmail.com>
2022-04-26 08:15:48 +0100

    artisan select jobs

--- artisan/views.py
+++ artisan/views.py
@@ -156,7 +156,7 @@ def jobAccepted(request,id):
 
        # OrderItem.objects.filter(id=id, ordered=True,status='Paid')
         accepted_job= OrderItem.objects.get(id=id, ordered=True,status='Paid')
-        OrderItem.objects.update(id=id, ordered=True,status='Paid',artisan_assigned=artisan)
+        OrderItem.objects.get(id=id, ordered=True,status='Paid').update(artisan_assigned=artisan)
         #for name in accepted_job:
          #   for n in name.artisan_assigned.all():
           #      n.artisan_assigned=artisan
