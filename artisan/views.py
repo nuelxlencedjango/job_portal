a71367daed9c56e378e393893fafa9489fc6e554
nuelxelence <nuelxelence@gmail.com>
2022-04-26 19:18:50 +0100

    artisan select jobs

--- artisan/views.py
+++ artisan/views.py
@@ -165,7 +165,7 @@ def jobAccepted(request,id):
        
    
         
-        context={'id':id,'accepted_job':accepted_job}#,'artisan_assigned':artisan_assigned}
+        context={'id':id,'accepted_job':accepted_job,'job':job}#,'artisan_assigned':artisan_assigned}
         return render(request,'artisans/accepted_job.html',context)               
                     
             #job.artisan_assigned.set(artisan) #=job_info.artisan_assigned.user.username
