ed3d03ff82481349fc6bed78a5864e3624b9536c
nuelxelence <nuelxelence@gmail.com>
2022-04-29 09:01:07 +0100

    artisan select jobs

--- static/js/addbutton.js
+++ static/js/addbutton.js
@@ -1,9 +1,8 @@
 
-const name = document.querySelector(".name");
-function changeColor(color) {
-  name.style.color = color;
-}
-
+//const name = document.querySelector(".name");
+//function changeColor(color) {
+  //name.style.color = color;
+//}
 let data=document.getElementById(".confirm")
 function myFunction() {
     data.style.visibility = "hidden";
