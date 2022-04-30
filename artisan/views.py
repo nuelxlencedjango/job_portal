87267937559741dec3b3b17ee98f0e0c0adc9e4c
nuelxelence <nuelxelence@gmail.com>
2022-04-30 03:43:41 +0100

    artisan complete

--- artisan/views.py
+++ artisan/views.py
@@ -223,7 +223,7 @@ def completeJob(request,id):
     artisan = Artisan.objects.get(user=request.user)
     orders=OrderItem.objects.filter(artisan_assigned =artisan, ordered=True,status='Paid',accepted ="Accepted")
 
-    
+    ord=OrderItem.objects.filter(artisan_assigned =artisan, ordered=True,status='Paid',accepted ="Accepted").last()
            
 
         
@@ -231,7 +231,7 @@ def completeJob(request,id):
     #bb=OrderItem.objects.get(artisan_assigned=Artisan.objects.get(user=request.user),pk=id,accepted='Accepted').last()
     #b=bb.user.last_name
    # n =bb.product.name
-    return render(request,'artisans/current_job.html',{'a':a,'n':nn,'artisan':artisan,'orders':orders})   
+    return render(request,'artisans/current_job.html',{'a':a,'n':nn,'artisan':artisan,'orders':orders,'ord':ord})   
     #if OrderItem.objects.filter(id=id,accepted='Accepted').exists():
      #   pass
         #OrderItem.objects.filter(id=id,accepted='Accepted').update(work_done=True)
