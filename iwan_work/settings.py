cdaf1bd8f98f63bd0e136f918f8e0d521fcbb3f9
OfferUp <offerup@OfferUps-MacBook-Pro.local>
2023-05-12 17:48:10 -0700

    whitenoise

--- iwan_work/settings.py
+++ iwan_work/settings.py
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'whitenoise.runserver_nostatic',
+
     'products',
     'cloudinary',
     'account',
@@ -62,7 +65,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
